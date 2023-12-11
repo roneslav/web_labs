1f487b7e948cce14375e5b97e25f06eeabeed931
roneslav <rostyslav.troian.ir.2022@lpnu.ua>
2023-12-12 00:30:51 +0200

    deleting lab11

--- lab11/src/components/formError/formError.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './formError.css'
-
-const FormError = ({ touched, error }) => {
-    return touched && error ? <div className="error">{error}</div> : null;
-};
-
-export default FormError;
